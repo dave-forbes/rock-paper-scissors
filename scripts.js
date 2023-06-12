6bebd164d28d789762c6a070b9cdd00857bb5359
David Forbes <davidforbesi@hotmail.com>
2023-06-12 15:47:12 +0100

    adding comments to make code easier to understand

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-let playerScore = 0;
+let playerScore = 0;            // create variables to keep track of the scores
 let computerScore = 0;
 
 function getComputerChoice() {
   let random = Math.floor((Math.random() * 100));
   if (random < 100 / 3) {
     return 'rock';
-  } else if (random > 100 / 3 && random < 200 / 3) {
+  } else if (random > 100 / 3 && random < 200 / 3) {            // function to determine what the computer chooses, it has a 1/3 chance of outputting rock, paper or scissors
     return 'paper';
   } else {
     return 'scissors';
@@ -15,14 +15,14 @@ function getComputerChoice() {
 function getPlayerChoice() {
   let choice = prompt('Rock, Paper or Scissors!').toLowerCase();
   if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
-    alert('please choose either rock, paper or scissors!');
+    alert('please choose either rock, paper or scissors!');                        //function to aks the player to input their choice (case insensitive) but not allow them to input anything other than rock, paper scissors
     return getPlayerChoice();
   } else if (choice == 'rock' || choice == 'paper' || choice == 'scissors') {
     return choice;
   }
 }
 
-function rockPaperScissors(playerSelection, computerSelection) {
+function rockPaperScissors(playerSelection, computerSelection) {              //function to play a round of rock, paper scissors, determines who won, returns results and adds to scores
 
   if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
     playerScore += 1;
@@ -35,7 +35,7 @@ function rockPaperScissors(playerSelection, computerSelection) {
   }
 }
 
-function scores(playerScore, computerScore) {
+function scores(playerScore, computerScore) {                   // function to tally up scores and display who won and final scores
   if (playerScore > computerScore) {
     return `Player wins by ${playerScore} to ${computerScore}!`;
   } else if (computerScore > playerScore) {
@@ -47,17 +47,17 @@ function scores(playerScore, computerScore) {
 
 const start = document.querySelector('button');
 
-start.addEventListener("click", () => {
+start.addEventListener("click", () => {                   // eventlistener to wait until button is pressed before looping through 5 rounds of rock paper scissors
   for (let i = 1; i <= 5; i++) {
     computerSelection = getComputerChoice();
     playerSelection = getPlayerChoice();
 
     let result = rockPaperScissors(playerSelection, computerSelection);
 
-    console.log(`Round ${i} results: `, result);
+    console.log(`Round ${i} results: `, result);        // display results after each round 
   } console.log(scores(playerScore, computerScore));
   playerScore = 0;
-  computerScore = 0;
+  computerScore = 0;                                    // reset scores back to 0 - 0 after 5 rounds
 });
 
 
