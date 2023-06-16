24b061b553ebff78afa58c7124233000dba04455
David Forbes <davidforbesi@hotmail.com>
2023-06-16 13:47:14 +0100

    created GUI

--- scripts.js
+++ scripts.js
@@ -1,64 +1,73 @@
-let playerScore = 0;            // create variables to keep track of the scores
+let playerScore = 0;
 let computerScore = 0;
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const playerChoice = document.querySelector('.player-choice');
+const roundResults = document.querySelector('.round-results');
+const finalScores = document.querySelector('.final-scores');
+const currentScores = document.querySelector('.scores');
+const start = document.querySelector('.play-round');
+const startAgain = document.querySelector('.play-again');
+let playerSelection = "";
+let counter = 0;
 
 function getComputerChoice() {
   let random = Math.floor((Math.random() * 100));
   if (random < 100 / 3) {
     return 'rock';
-  } else if (random > 100 / 3 && random < 200 / 3) {            // function to determine what the computer chooses, it has a 1/3 chance of outputting rock, paper or scissors
-    return 'paper';
+  } else if (random > 100 / 3 && random < 200 / 3) {
   } else {
     return 'scissors';
   }
 }
 
-function getPlayerChoice() {
-  let choice = prompt('Rock, Paper or Scissors!').toLowerCase();
-  if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
-    alert('please choose either rock, paper or scissors!');                        //function to aks the player to input their choice (case insensitive) but not allow them to input anything other than rock, paper scissors
-    return getPlayerChoice();
-  } else if (choice == 'rock' || choice == 'paper' || choice == 'scissors') {
-    return choice;
-  }
-}
-
-function rockPaperScissors(playerSelection, computerSelection) {              //function to play a round of rock, paper scissors, determines who won, returns results and adds to scores
-
+function rockPaperScissors(playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
     playerScore += 1;
-    return `You Won! ${playerSelection} beat ${computerSelection}`
+    return `You Won! Computer chose ${computerSelection}!`
   } else if (computerSelection == "rock" && playerSelection == "scissors" || computerSelection == "paper" && playerSelection == "rock" || computerSelection == "scissors" && playerSelection == "paper") {
     computerScore += 1;
-    return `You Lost! ${computerSelection} beat ${playerSelection}`
+    return `You Lost! Computer chose ${computerSelection}!`
   } else {
-    return `Nobody won! Neither ${computerSelection} nor ${playerSelection} beat each other!`
+    return `Nobody won! Computer chose ${computerSelection} as well!`
   }
 }
 
-function scores(playerScore, computerScore) {                   // function to tally up scores and display who won and final scores
+function scores(playerScore, computerScore) {
   if (playerScore > computerScore) {
-    return `Player wins by ${playerScore} to ${computerScore}!`;
+    return `Player wins by ${playerScore} - ${computerScore}!`;
   } else if (computerScore > playerScore) {
-    return `Computer wins by ${computerScore} to ${playerScore}!`;
+    return `Computer wins by ${computerScore} - ${playerScore}!`;
   } else {
-    return `It's a draw! The scores were tied at ${playerScore} and ${computerScore}! `;
+    return `It's a draw! The scores were tied at ${playerScore} - ${computerScore}! `;
   }
 }
 
-const start = document.querySelector('button');
+function playRound() {
+  if (playerSelection == '') return playerChoice.textContent = 'Please choose before playing.'
+  counter++
+  computerSelection = getComputerChoice();
+  let result = rockPaperScissors(playerSelection, computerSelection);
+  const content = document.createElement('p');
+  content.textContent = `Round ${counter}: ` + result;
+  currentScores.textContent = `Player is on ${playerScore}` + ` and Computer is on ${computerScore}`;
+  roundResults.appendChild(content);
+  if (counter == 5) {
+    start.disabled = true;
+    finalScores.textContent = scores(playerScore, computerScore);
+  }
+}
 
-start.addEventListener("click", () => {                   // eventlistener to wait until button is pressed before looping through 5 rounds of rock paper scissors
-  for (let i = 1; i <= 5; i++) {
-    computerSelection = getComputerChoice();
-    playerSelection = getPlayerChoice();
+rock.addEventListener('click', () => playerSelection = 'rock');
+rock.addEventListener('click', () => playerChoice.textContent = 'You have chosen rock');
+paper.addEventListener('click', () => playerSelection = 'paper');
+paper.addEventListener('click', () => playerChoice.textContent = 'You have chosen paper');
+scissors.addEventListener('click', () => playerSelection = 'scissors');
+scissors.addEventListener('click', () => playerChoice.textContent = 'You have chosen scissors')
+start.addEventListener("click", playRound);
+startAgain.addEventListener('click', () => { location.reload() });
 
-    let result = rockPaperScissors(playerSelection, computerSelection);
-
-    console.log(`Round ${i} results: `, result);        // display results after each round 
-  } console.log(scores(playerScore, computerScore));
-  playerScore = 0;
-  computerScore = 0;                                    // reset scores back to 0 - 0 after 5 rounds
-});
 
 
 
